e04c3cdb4983851021f004a6fcf59553c7746004
Ganesh K <ganesh.khairnar@synechron.com>
2020-07-03 20:54:33 +0530

    commit # 2

--- src/test/java/stepDefinitions/stepDefinitions.java
+++ src/test/java/stepDefinitions/stepDefinitions.java
@@ -16,142 +16,107 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 public class stepDefinitions {
-	RequestSpecification requestSpec;
+	RequestSpecification request;
 	Response response;
 	ValidatableResponse validatableResponse;
 
+	String ratesapi_URL = "https://api.ratesapi.io/api/";
 	public int statcode;
 	public String endpoint, currency;
 
-	public stepDefinitions() {
-		RestAssured.baseURI = "https://api.ratesapi.io";
-		RestAssured.basePath = "/api";
-	}
-
 	@Given("Rates API for Latest Foreign Exchange rates")
 	public void rates_API_for_Latest_Foreign_Exchange_rates() {
-		// requestSpec = RestAssured.given();
-		response = given().when().get("/latest/");
-		// given().when().get("https://api.ratesapi.io/api/latest/").then().log().body();
-	}
+		request = given();
+			}
 
-	@When("The Rates API is available")
-	public void the_Rates_API_is_available() {
-		assertEquals(response.getStatusCode(), 200);
-		// response.prettyPrint();
+	@When("The Rates API is available for {string}")
+	public void the_Rates_API_is_available(String strendpoint) {
+		response = request.when().get(ratesapi_URL + strendpoint);
+		// System.out.println("response: " + response.prettyPrint());
 
 	}
 
-	@Then("The Response is sucesssful with status code")
-	public void the_Response_is_sucesssful_with_status_code() {
-		System.out.println("........ The Rates API is live.....");
+	@Then("The Response is sucesssful with status code {int}")
+	public void the_Response_is_sucesssful_with_status_code(int statusCode) {
 
-		response.then().log().body();
-
+		assertEquals(statusCode, response.getStatusCode());
+		
 		System.out.println("# : The Response is successful with the Status code - 200");
 		System.out.println("--------------------------------------------------------------");
 		// response.prettyPrint();
 	}
 
-	@Then("Validate the response with assertions")
-	public void validate_the_response_with_assertions() {
-
-		// response = given().get("https://api.ratesapi.io/api/latest/");
+	@Then("Validate the response for base {string}")
+	public void validate_the_response_for_base(String baseval) {
 
 		ResponseBody body = response.getBody();
-		// Get Response Body as String
 		String bodyStr = body.asString();
 		assertTrue(bodyStr.contains("base"));
 		JsonPath jsextractor = response.jsonPath();
-		//extract specific element from JSON document
 		String base = jsextractor.get("base");
-		currency = "EUR";
-		// check if the specific JSON element is equal to expected value
-		assertTrue(base.equalsIgnoreCase(currency));
+
+		assertTrue(base.equalsIgnoreCase(baseval));
 		response.then().log().all();
 
-		System.out.println("# :Verified the Response with assertions shows base=" + currency);
+		System.out.println("# :Verified the Response with assertion - base=" + baseval);
 		System.out.println("--------------------------------------------------------------");
+		currency = baseval;
 	}
 
 	@When("An incorrect or incomplete Url {string} is provided")
 	public void an_incorrect_or_incomplete_url_something_is_provided(String strArg1) {
-
-		//RestAssured.baseURI = "https://api.ratesapi.io";
-		//RestAssured.basePath = "/api";
+		response = request.when().get(ratesapi_URL + strArg1);
 		endpoint = strArg1;
-		response = given().get(strArg1);
-		statcode = response.getStatusCode();
-		// assertEquals(response.getStatusCode(), 404);
-
 	}
 
-	@Then("incorrect URI error is displayed")
-	public void incorrect_URI_error_is_displayed() {
+	@Then("Shows the {int} for incorrect or invalid url")
+	public void shows_the_for_incorrect_or_invalid_url(int statcode) {
+		if (statcode == response.getStatusCode()) {
+			assertEquals(statcode, response.getStatusCode());
+			response.then().log().all();
+			System.out.println("Please enter the valid endpoint for correct results");
 
-		if (statcode == 400) {
-			given().when().get(endpoint).then().log().ifStatusCodeIsEqualTo(400);
-			// System.out.println("Error: Requested URL is not found");
-			System.out.println("Please provide the correct endpoint [example:/latest,/latest?base=USD]");
-
-		} else if (statcode == 404) {
-			given().when().get(endpoint).then().log().ifStatusCodeIsEqualTo(404);
-			// System.out.println("\"error\": \"time data " + endpoint + " does not match
-			// format '%Y-%m-%d'\"");
-			System.out.println("Please provide the correct endpoint [example:/latest,/latest?base=USD]");
-
+		} else if (statcode == response.getStatusCode()) {
+			assertEquals(statcode, response.getStatusCode());
+			response.then().log().all();
+			System.out.println("Please enter the valid endpoint for correct results");
+		} else {
+			// response.then().log().body();
+			System.out.println("The Rates API URL is valid");
 		}
 
-		else {
-			System.out.println("Requested Rates API URL is correct");
-		}
-		System.out.println("--------------------------------------------------------------");
 		System.out.println("# : Verified the error message for incorrect endpoint in the Url");
 		System.out.println("--------------------------------------------------------------");
+
 	}
 
-	
-	
 	@Given("Rates API for specific date Foreign Exchange rates")
 	public void rates_API_for_specific_date_Foreign_Exchange_rates() {
-		response = given().when().get("/2020-07-03");
-
-		
+		request = given().contentType(ContentType.JSON);
+	
 	}
 
 	@When("An incorrect date {string} is provided")
 	public void an_incorrect_date_is_provided(String strArg2) {
+		response = request.when().get(ratesapi_URL + strArg2);
 		endpoint = strArg2;
-		response = given().get(strArg2);
-		statcode = response.getStatusCode();
 	}
 
-	@Then("incorrect date format error message is displayed")
-	public void incorrect_date_format_error_message_is_displayed() {
-
-		if (statcode == 400) {
-			given().when().get(endpoint).then().log().ifStatusCodeIsEqualTo(400);
+	@Then("Shows the {int} for incorrect date")
+	public void shows_the_for_incorrect_date(int statcode) {
+		if (statcode == response.getStatusCode()) {
+			assertEquals(statcode, response.getStatusCode());
+			response.then().log().body();
 			System.out.println("Please enter the valid date for correct results");
-
-		} else if (statcode == 404) {
-			given().when().get(endpoint).then().log().ifStatusCodeIsEqualTo(404);
-			System.out.println("Please enter the valid date for correct results");
+		} else {
+			//response.then().log().body();
+			System.out.println("Status Code :"+ (response.getStatusCode()));
+			System.out.println("Today's rates will be displayed, eventhough specified date is future date");
 		}
 
-		else {
-			given().when().get(endpoint).then().log().body();
-			System.out.println("The Date spefied is either current / future date");
-		}
-		System.out.println("--------------------------------------------------------------");
 		System.out.println("# : Verified the error message for incorrect date in the Url");
 		System.out.println("--------------------------------------------------------------");
-	
+
 	}
 
-
-	
-	
-	
-	
-	
 } // end of class
--- target/test-classes/features/ratesapi.feature
+++ target/test-classes/features/ratesapi.feature
@@ -2,32 +2,42 @@ Feature: Validating Rates API
 
 Scenario: Verify Rates API response returns success status code 
 	Given Rates API for Latest Foreign Exchange rates 
-	When The Rates API is available 
-	Then The Response is sucesssful with status code 
+	When The Rates API is available for "latest" 
+	Then The Response is sucesssful with status code 200 
 	
 Scenario: Verify Rates API response with assertions 
 	Given Rates API for Latest Foreign Exchange rates 
-	When The Rates API is available 
-	Then Validate the response with assertions 
+	When The Rates API is available for "latest" 
+	Then Validate the response for base "EUR" 
 	
-Scenario: Verify error is displayed when incorrect Rates API Url is submitted 
+Scenario Outline:Verify error is displayed when incorrect Rates API Url is submitted 
 	Given Rates API for Latest Foreign Exchange rates 
-	When An incorrect or incomplete Url "/lat" is provided 
-	Then incorrect URI error is displayed 
-
-	
+	When An incorrect or incomplete Url <endpoint> is provided 
+	Then Shows the <statcode> for incorrect or invalid url 
+	Examples: 
+		|endpoint|statcode|
+		|"lates" |  400   |
+		|"latest/345" |  404   |
+		|"latest/ghf" |  404   |
+		
 Scenario: Verify Rates API for specific date returns success status code 
 	Given Rates API for specific date Foreign Exchange rates 
-	When The Rates API is available 
-	Then The Response is sucesssful with status code 
+	When The Rates API is available for "2019-12-05" 
+	Then The Response is sucesssful with status code 200 
 	
 Scenario: Verify Rates API for specific date shows asserted response 
 	Given Rates API for specific date Foreign Exchange rates 
-	When The Rates API is available 
-	Then Validate the response with assertions 
+	When The Rates API is available for "2020-01-05" 
+	Then Validate the response for base "EUR" 
 	
-Scenario: Verify response when future or incorrect date is submitted to API 
+Scenario Outline: Verify response when future or incorrect date is submitted to API 
 	Given Rates API for specific date Foreign Exchange rates 
-	When An incorrect date "/2021-01-01" is provided 
-	Then incorrect date format error message is displayed 
-	
+	When An incorrect date <vardate> is provided 
+	Then Shows the <statcode> for incorrect date
+	Examples: 
+		|vardate|statcode|
+		|"2010-01-35" |  400   |
+		|"2020-01-" |  400   |
+		|"01-05-2019" |  400   |
+		|"2020-10-15" |  200   |
+		
